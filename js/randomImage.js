988e1836fa697aea360ea6a55f8b1a2c5cda2b69
Claudia H <claudia-isabel.huuse@tktk.ee>
2022-11-02 17:25:06 +0200

    JS Sandbox

---
+++ js/randomImage.js
@@ -0,0 +1,11 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg"];
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage () {
+    let randomIndex = Math.floor(Math.random() * images.length);
+    console.log(randomIndex);
+    image.src = images[randomIndex];
+}
